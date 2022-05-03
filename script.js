9406199b3168ea457fc1a38364310bb9aa0646b7
João Pedro Monção <87674021+jpmoncao@users.noreply.github.com>
2022-05-03 20:38:58 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function mostrarLevels() {
+    const elementCard = document.getElementsByClassName("card");
+    const converted = { ".card": { visibility: "hidden" } }
+
+    elementCard.style = converted
+}
