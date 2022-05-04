dcd3b32481d02843a5cba5571493b8cdea7301d3
João Pedro Monção <pedrohjoao44@gmail.com>
2022-05-04 16:56:47 -0300

    landing page pronta

--- script.js
+++ script.js
@@ -20,32 +20,3 @@ function mostrarJogoDaVelha() {
     (document.getElementById("jogodavelha")).style.visibility = "visible"
 
 }
-
-function verificarOpcoes() {
-    const elementsRadioInput = document.getElementsByName("opcoes");
-
-    for (var i = 0; i < elementsRadioInput.length; i++) {
-        if (elementsRadioInput[i].checked) {
-            return elementsRadioInput[i].value;
-        }
-    }
-    /*if (elementRadioInput1.checked || elementRadioInput2.checked) {
-        (document.getElementsByClassName("btn-opcoes")).disabled = false
-    }
-
-    if (elementRadioInput1){}*/
-}
-
-function verificarEscolha() {
-    const elementRadioInput1 = document.getElementById("radio1");
-    const elementRadioInput2 = document.getElementById("radio2");
-    const escolha = verificarOpcoes();
-
-    console.log(escolha)
-    if (escolha == 1) {
-        mostrarLevels()
-    }
-    else if (escolha == 2) {
-        mostrarJogoDaVelha()
-    }
-}
